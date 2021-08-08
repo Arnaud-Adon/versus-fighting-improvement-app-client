0fd2913ee11ccb9ebbfb74e9569628b8b10df30d
Arnaud-Adon <assy.adon@yahoo.fr>
2021-08-08 22:25:01 +0200

    refactor: Refonte des character et des notes associées

--- screens/ImproveScreen.js
+++ screens/ImproveScreen.js
@@ -1,39 +1,68 @@
-import React from "react";
-import { StyleSheet, Text, View, Image } from "react-native";
-import versusLogo from "../assets/images/screen/vs.png";
-import OpponentCharacter from "../components/Character/OpponentCharacter";
-import UserCharacter from "../components/Character/UserCharacter";
+import React, { useContext, useEffect, useState } from "react";
+import { StyleSheet, View, ActivityIndicator } from "react-native";
 import OpponentNote from "../components/Note/OpponentNote";
 import UserInfo from "../components/User/UserInfo";
+import VersusCharacter from "../components/Character/VersusCharacter";
+import { LinearGradient } from "expo-linear-gradient";
+import { Colors } from "../lib/utils/colors";
+import { connect } from "react-redux";
+import {
+  getCharacters,
+  getUser,
+  getUserCharacters,
+} from "../lib/state/selectors";
+import { useSelectCharacterContext } from "../lib/context/selectCharacterContext";
 
-const ImproveScreen = () => {
-  return (
-    <View testID="improve-screen">
-      <View style={styles.user}>
-        <UserInfo />
-        <UserCharacter />
-      </View>
-      <View testID="improve-screen-logo" style={styles.image}>
-        <Image source={versusLogo} />
-      </View>
-      <View style={styles.opponent}>
-        <OpponentCharacter />
-        <OpponentNote />
-      </View>
-    </View>
-  );
+const ImproveScreen = ({ user, characters, userCharacters }) => {
+  const {
+    userCharacter,
+    opponentCharacter,
+    setUserCharacter,
+    setOpponentCharacter,
+  } = useSelectCharacterContext();
+
+  useEffect(() => {
+    setUserCharacter(userCharacters[0]);
+  }, [userCharacters]);
+
+  useEffect(() => {
+    setOpponentCharacter(characters[0]);
+  }, [characters]);
+
+  if (!userCharacter && !opponentCharacter) {
+    return <ActivityIndicator size="large" />;
+  } else {
+    return (
+      <LinearGradient
+        style={styles.container}
+        colors={[Colors.DARKER_RED, Colors.DARKER_BLUE, Colors.LIGHTER_BLUE]}
+        testID="improve-screen"
+      >
+        <View style={styles.user}>
+          <UserInfo user={user} />
+        </View>
+        <View>
+          <VersusCharacter
+            character={userCharacter}
+            opponent={opponentCharacter}
+          />
+        </View>
+        <View style={styles.opponent}>
+          <OpponentNote />
+        </View>
+      </LinearGradient>
+    );
+  }
 };
 
-export default ImproveScreen;
+export default connect((state) => ({
+  user: getUser(state),
+  characters: getCharacters(state),
+  userCharacters: getUserCharacters(state),
+}))(ImproveScreen);
 
 const styles = StyleSheet.create({
   container: {
-    flexDirection: "column",
+    flex: 1,
   },
-  user: {},
-  image: {
-    width: 100,
-    height: 120,
-  },
-  opponent: {},
 });
